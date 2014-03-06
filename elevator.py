70c8fdd277a9616de20988422d14f587f52d5e78
Martin Skow Røed <msroed@gmail.com>
2014-03-06 12:15:44 +0100

    changed into thrads

--- elevator.py
+++ elevator.py
@@ -1,136 +1,49 @@
+from signalpoller import SignalPoller
 from channels import INPUT, OUTPUT
-from IO import io
-from time import sleep
-from models import Panel, OrderQueue, Order, STATE, SIGNAL, DoorTimer
+from functools import partial
+from io import IO
 
 class Elevator:
 	def __init__(self):
-		self.panel = Panel()
-		self.orderQueue = OrderQueue()
-		self.direction = OUTPUT.MOTOR_UP
-		self.currentFloor=1
-		self.currentState = STATE.IDLE
-		self.nextState = STATE.IDLE
-		self.signals = [False] * SIGNAL.NUM_SIGNALS
-		self.NUM_FLOORS = INPUT.NUM_FLOORS
-		self.speed = 100
-		self.drive()
-		self.moving = True
-		while self.panel.get_floor() == -1:
-			pass
-		self.currentFloor = self.panel.get_floor()
-		self.stop()
+		self.signalPoller = SignalPoller()
+		self.direction = OUTPUT.MOTOR_DOWN
 		self.moving = False
-		self.speed = 300
 
-	def run(self):
-		while not self.panel.get_stop_signal():
-			self.nextState = self.currentState
+	def set_floor_callbacks(self):
+		""" Decide what will happen when floor changes """
+		for floor, channel in enumerate(INPUT.SENSORS):
+			self.signalPoller.add_callback_to_channel(channel, partial(self.set_floor_indicator, floor))
 
-			if self.currentState == STATE.IDLE:
-				if self.signals[SIGNAL.HAS_ORDERS]:
-					if self.signals[SIGNAL.SHOULD_STOP]:
-						self.nextState = STATE.OPEN_DOOR
-					else: self.nextState = STATE.DRIVE
+	def set_button_callbacks(self):
+		""" Set what will happen when button is pressed """
+		for floor, channel in enumerate(INPUT.IN_BUTTONS):
+			self.signalPoller.add_callback_to_channel(channel, partial(self.button_pressed_callback, "IN", floor))
 
-			elif self.currentState == STATE.DRIVE:
-				if self.signals[SIGNAL.SHOULD_STOP]:
-					self.nextState = STATE.OPEN_DOOR
+		for floor, channel in enumerate(INPUT.UP_BUTTONS):
+			self.signalPoller.add_callback_to_channel(channel, partial(self.button_pressed_callback, "UP", floor))
 
-			elif self.currentState == STATE.OPEN_DOOR:
-				if self.signals[SIGNAL.TIMER_FINISHED]:
-					self.nextState = STATE.CLOSE_DOOR
-
-			elif self.currentState == STATE.CLOSE_DOOR:
-				if self.signals[SIGNAL.OBSTRUCTION]:
-					self.nextState = STATE.OPEN_DOOR
-				elif self.signals[SIGNAL.HAS_ORDERS]:
-					self.nextState = STATE.DRIVE
-
-			if self.nextState != self.currentState:
-				if self.nextState == STATE.DRIVE:
-					self.drive()
-
-				elif self.nextState == STATE.OPEN_DOOR:
-					print "YO KIDS"
-					if self.currentState == STATE.DRIVE:
-						self.stop()
-					self.orderQueue.delete_order_in_floor(self.currentFloor)
-					self.panel.turn_off_lights_in_floor(self.currentFloor)
-					self.panel.set_door_light(1)
-					self.panel.timer.start()
-
-				elif self.nextState == STATE.CLOSE_DOOR:
-					self.panel.set_door_light(0)
-
-				elif self.nextState == STATE.EMERGENCY_STOP:
-					self.stop()
-
-			self.currentState = self.nextState
-			self.update_signals()
-			order = self.panel.get_order(self.currentFloor)
-			if order:
-				self.orderQueue.add_order(order)
-		self.stop()
+		for floor, channel in enumerate(INPUT.DOWN_BUTTONS):
+			self.signalPoller.add_callback_to_channel(channel, partial(self.button_pressed_callback, "DOWN", floor))
 
 
+	def turn_off_lights_in_floor(self, floor):
+		""" Turn of all light in a certain floor """
+		self.set_button_lamp(floor, OUTPUT.UP_LIGHTS, 0)
+		self.set_button_lamp(floor, OUTPUT.DOWN_LIGHTS, 0)
+		self.set_button_lamp(floor, OUTPUT.IN_LIGHTS, 0)
 
-
-	def update_signals(self):
-		if self.currentState == STATE.IDLE:
-			self.signals[SIGNAL.HAS_ORDERS] = self.orderQueue.has_orders()
-			self.signals[SIGNAL.SHOULD_STOP] = self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_UP, self.currentFloor) or self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_DOWN, self.currentFloor)
-
-		elif self.currentState == STATE.DRIVE:
-			tempFloor = self.panel.get_floor()
-			if tempFloor != -1:
-				self.currentFloor = tempFloor
-				self.panel.set_floor_indicator(self.currentFloor)
-				if self.orderQueue.has_order_in_floor(self.direction, self.currentFloor) or self.find_direction() != self.direction:
-					self.signals[SIGNAL.SHOULD_STOP] = 1
-				else: self.signals[SIGNAL.SHOULD_STOP] = 0
-			else: self.signals[SIGNAL.SHOULD_STOP] = 0
-
-		elif self.currentState == STATE.OPEN_DOOR:
-			self.signals[SIGNAL.TIMER_FINISHED] = self.panel.timer.is_finished
-
-		elif self.currentState == STATE.CLOSE_DOOR:
-			self.signals[SIGNAL.HAS_ORDERS] = self.orderQueue.has_orders()
-			self.signals[SIGNAL.OBSTRUCTION] = self.panel.get_obstruction_signal()
-
-		self.signals[SIGNAL.EMERGENCY_STOP] = self.panel.get_stop_signal()
-
-
-
-	def find_direction(self):
-		if self.direction == OUTPUT.MOTOR_UP:
-			for floor in xrange(self.currentFloor+1, self.NUM_FLOORS):
-			   if self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_UP, floor) or self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_DOWN, floor):
-					return OUTPUT.MOTOR_UP
-			return OUTPUT.MOTOR_DOWN
+	def set_floor_indicator(self, floor):
+		""" Set floor indicators based on floor 
+		@input floor
+		"""
+		if floor & 0x01:
+			io.set_bit(OUTPUT.FLOOR_IND1, 1)
 		else:
-			for floor in xrange(self.currentFloor-1, -1, -1):
-				if self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_UP, floor) or self.orderQueue.has_order_in_floor(OUTPUT.MOTOR_DOWN, floor):
-					return OUTPUT.MOTOR_DOWN
-			return OUTPUT.MOTOR_UP
-		return OUTPUT.MOTOR_UP
-
-	def drive(self):
-		self.direction = self.find_direction()
-
-		io.set_bit(OUTPUT.MOTORDIR, self.direction)
-		io.write_analog(OUTPUT.MOTOR, 2048+4*abs(self.speed))
-		self.moving = True
-
-	def stop(self):
-		if not self.moving:
-			return
-		if self.direction == OUTPUT.MOTOR_UP:
-			io.set_bit(OUTPUT.MOTORDIR, OUTPUT.MOTOR_DOWN)
+			io.set_bit(OUTPUT.FLOOR_IND1, 0)
+		if floor & 0x02:
+			io.set_bit(OUTPUT.FLOOR_IND2, 1)
 		else:
-			io.set_bit(OUTPUT.MOTORDIR, OUTPUT.MOTOR_UP)
+			io.set_bit(OUTPUT.FLOOR_IND2, 0)
 
-		sleep(0.02)
-		io.write_analog(OUTPUT.MOTOR, 2048)
-
-	
+	def button_pressed_callback(self, button_type, floor):
+		print button_type, direction, floor
---
+++ signalpoller.py
@@ -0,0 +1,30 @@
+from threading import Thread
+from time import sleep
+from channels import INPUT, OUTPUT
+from IO import io
+
+
+class SignalPoller(Thread):
+
+	def __init__(self):
+		super(SignalPoller, self).__init__()
+		self.callbacks = {}
+		self.running = None
+		self.frequency = 1000.0
+
+	def add_callback_to_channe(self, channel, callback):
+		""" Fires the callback when the value on the channel changes """
+		self.callbacks[channel] = {'lastval': None, 'callback': callback}
+
+	def run(self):
+		""" Run the poller until stop is called """
+		self.running = True
+		while self.running:
+			sleep(1/self.frequency)
+			for channel in INPUT.ALL:
+				value = io.read_bit(channel)
+				if value != self.callbacks[channel]['lastval']:
+					self.callbacks[channel]['callback']()
+
+	def stop(self):
+		self.running = False
